903a3a7db60b27fdfc9e6586268cb2ee14baeade
Audrey Paleo <a.zini@laposte.net>
2013-12-11 11:57:40 +0100

    finir le code pour les 2 memoires lignes

--- vhd_mouvement/module_roberts.vhd
+++ vhd_mouvement/module_roberts.vhd
@@ -50,11 +50,15 @@ signal pixel_lig_prec			: std_logic_vector(7 downto 0) ;
 signal pixel_prec			: std_logic_vector(7 downto 0) ;
 signal address_cur			: std_logic_vector(address_size-1 downto 0) ;
 signal sig_data_out			: std_logic_vector(7 downto 0) ;
-signal read_write			: std_logic;
+signal sig_G				: std_logic_vector(15 downto 0) ; --attention : multiplication !
+signal read_write_pix			: std_logic;
+signal read_write_grad1			: std_logic;
+signal read_write_grad2			: std_logic;
+signal rw_grad					: std_logic;
 
 begin
 
-	u1: module_memoire_ligne
+	mem_ligne_pix: module_memoire_ligne
 	
 	generic map(
 		address_size => address_size
@@ -66,10 +70,42 @@ begin
 		address	=> address_cur,
 		data_in	=> iY,
 		data_out => sig_data_out,
-		read_write => read_write
+		read_write => read_write_pix
 	);
 	
 	
+	mem_ligne_grad1: module_memoire_ligne
+	
+	generic map(
+		address_size => address_size
+	)
+		
+	port map(
+		CLK => CLK,
+		RESET => RESET,			
+		address	=> address_cur,
+		data_in	=> sig_G,
+		data_out => sig_data_out_g1,
+		read_write => read_write_grad1
+	);
+	
+	
+	mem_ligne_grad2: module_memoire_ligne
+	
+	generic map(
+		address_size => address_size
+	)
+		
+	port map(
+		CLK => CLK,
+		RESET => RESET,			
+		address	=> address_cur,
+		data_in	=> sig_G,
+		data_out => sig_data_out_g2,
+		read_write => read_write_grad2
+	);
+	
+
 
 
 	process_roberts : process(CLK, RESET) --signal iY was modified : incoming pixel
@@ -79,6 +115,9 @@ begin
 	variable var_pixel_lig_prec : std_logic_vector(7 downto 0) ;
 	variable var_pixel_prec : std_logic_vector(7 downto 0) ;
 	variable var_oY : std_logic_vector(7 downto 0) ;
+	variable var_Gv : std_logic_vector(7 downto 0) ;
+	variable var_Gh : std_logic_vector(7 downto 0) ;
+	
 	
 	
 	begin
@@ -86,10 +125,10 @@ begin
 			if in_active_area = '1' then
 				
 				if RESET = '0' then 
-					read_write <='0';
+					read_write_pix <='0';
 				  address_cur <="00000000";
 			
-				elsif read_write = '0' then -- on va faire une lecture en memoire_ligne
+				elsif read_write_pix = '0' then -- on va faire une lecture en memoire_ligne
 				
 					-- mise à jour des variables :
 					var_data_out := sig_data_out;
@@ -98,23 +137,43 @@ begin
 					var_pixel_prec := pixel_prec;
 				
 					-- Calcul du filtrage :
-					var_oY := iY + sig_data_out - pixel_lig_prec - pixel_prec;
-				
-				
+					var_Gh := iY + sig_data_out - pixel_lig_prec - pixel_prec;
+					var_Gv := -iY + sig_data_out + pixel_lig_prec - pixel_prec;
+					sig_G <= var_Gv*var_Gv + var_Gh*Var_Gh;		--TODO : verifier que tout ca tient en un cycle			
+									
 					-- mise a jour des signaux mais effectif en sortant du process
-					pixel_lig_prec <= var_data_out; --valeur future de pixel_lig_prec = valeur presente de sig_data_out
-					pixel_prec <= var_data_in; --valeur future de pixel_prec = valeur presente de iY
-				
+					pixel_lig_prec <= var_data_out; 
+					pixel_prec <= var_data_in; 
 
+					pixel_lig_prec2_g1 <= pixel_lig_prec1_g1
+					pixel_lig_prec2_g2 <= pixel_lig_prec1_g2
+					pixel_lig_prec1_g1 <= data_out_g1
+					pixel_lig_prec1_g2 <= data_out_g2
+					pixel_prec_g1 <= data_in
+					pixel_prec_g2 <= data_in
+					
+					
 					oY <= var_oY;
-					read_write <='1';
+					read_write_pix <='1'; --on ecrit oY dans la memoire_ligne_pix
+					
+					if rw_grad == '0' then --dans quelle memoire grad faut-il ecrire au prochain cycle
+						read_write_grad1 <= '1';
+					else
+						read_write_grad2 <= '1';
+					end
+					
 				
 					
-				elsif read_write = '1' then -- on va faire une ecriture en memoire_ligne
+				elsif read_write_pix = '1' then -- on va faire une ecriture en memoire_ligne
 					
 					--iY est ecrit en memoire_ligne via data_in					
 					address_cur <= address_cur+1;	
-					read_write <='0';
+					read_write_pix <='0';
+					
+					read_write_grad1 <= '0';
+					read_write_grad2 <= '0';
+					
+					
 		
 				end if;	
 				
